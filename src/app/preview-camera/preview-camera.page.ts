dbb50ac73d657db5e53921fcb23423b99c3b6966
Bunsarak PEN <penbunsarak@gmail.com>
2020-04-16 16:25:38 +0100

    preview camera

--- src/app/preview-camera/preview-camera.page.ts
+++ src/app/preview-camera/preview-camera.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CameraPreview } from '@ionic-native/camera-preview/ngx';
 @Component({
   selector: 'app-preview-camera',
   templateUrl: './preview-camera.page.html',
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PreviewCameraPage implements OnInit {
 
-  constructor() { }
+  smallPreview: boolean;
+  IMAGE_PATH: any;
+  colorEffect = 'none';
+  setZoom = 1;
+  flashMode = 'off';
+  isToBack = false;
+
+  constructor(private cameraPreview: CameraPreview) { }
 
   ngOnInit() {
   }
+
+  startCameraAbove() {
+    this.isToBack = false;
+    this.cameraPreview.startCamera({x: 80, y: 450, width: 250, height: 300, toBack: false, previewDrag: true, tapPhoto: true});
+  }
+
+
 
 }
